5eb221f52bca21438bd8f2c742aa5fd1ef301d9f
Audarya Uttarwar <audiuttarwar2000@gmail.com>
2020-08-10 21:37:24 +0530

    Added Switch codes

---
+++ Java/Codes/SwitchDemo/IntDemo.java
@@ -0,0 +1,20 @@
+class IntDemo {
+
+	public static void main(String[] args) {
+		int ch = 2;
+		switch(ch) {
+			case 1:
+				System.out.println("One");
+				break;
+			case 2:
+				System.out.println("Two");
+				break;
+			case 3:
+				System.out.println("Three");
+				break;
+			case 4:
+				System.out.println("Four");
+				break;
+		}
+	}
+}
---
+++ Java/Codes/SwitchDemo/charDemo.java
@@ -0,0 +1,16 @@
+class charDemo {
+	public static void main(String[] args) {
+		char ch = 'B';
+		switch(ch) {
+			case 65:
+				System.out.println("A char");
+				break;
+			case 66:
+				System.out.println("B char");
+				break;
+			case 67d:
+				System.out.println("C char");
+				break;
+		}
+	}
+}
