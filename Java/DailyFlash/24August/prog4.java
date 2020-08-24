13bd786b3e6fcd158e1b90a34d4190587d0739a5
Audarya Uttarwar <audiuttarwar2000@gmail.com>
2020-08-24 17:37:10 +0530

    24 august daily flash done

---
+++ Java/DailyFlash/24August/prog4.java
@@ -0,0 +1,11 @@
+class Prog4 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 100; i++) {
+			if(i % 2 == 0)
+				System.out.print(i + " ");
+		}
+		System.out.println();
+	}
+}
---
+++ Java/DailyFlash/24August/prog5.java
@@ -0,0 +1,13 @@
+class Prog5 {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 4; j++) {
+				System.out.print("0 ");	
+			}
+			System.out.println();
+		}
+		
+	}
+}
